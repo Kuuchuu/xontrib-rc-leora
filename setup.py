47e13020695256bcccd23c767151f84a9aa08c5d
Leora <Leora@Cat.Degree>
2024-10-14 23:03:29 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ except (IOError, OSError):
 
 setuptools.setup(
     name='xontrib-rc-leora',
-    version='0.15.1.0',
+    version='0.15.1.1',
     license='MIT',
     author='anki-code',
     author_email='no@no.no',
